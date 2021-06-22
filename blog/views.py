86d4d1c90c3fbaf0ae7f94fd7b9272223818e250
M010K <1223987303@qq.com>
2021-06-22 22:25:37 +0800

    add views of blog models

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+import re
+
+import markdown
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, DetailView
+from markdown.extensions.toc import TocExtension, slugify
+
+from .models import Category, Tag, Post
+
 
 # Create your views here.
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+
+    def get_context_data(self, **kwargs):  # 重写get_context_data方法
+        context = super().get_context_data(**kwargs)
+        # context['post_list'] = Post.objects.all().order_by('-created_time')
+        context['category_list'] = Category.objects.all().order_by('-id')
+        return context
+
+
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class TagView(ListView):
+    model = Tag
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tag=tag)
+
+
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，需要将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+
+        # 判断是否有目录结构
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+
+        return post
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+def feature(request):
+    return render(request, 'blog/styles.html')
