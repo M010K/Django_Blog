8175f07f84b41566bf2d5f70c781d2fc2da22bc7
M010K <1223987303@qq.com>
2021-06-22 23:10:18 +0800

    add archive url pattern

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('categories/<int:pk>/', views.CategoryView.as_view(), name='category'),
     path('tags/<int:pk>/', views.TagView.as_view(), name='tag'),
+    path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
     path('about.html', views.about, name='about'),
     path('contact.html', views.contact, name='contact'),
