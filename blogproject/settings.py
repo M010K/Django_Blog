ff5f6c1685b60b20f44948f5b0ac2d3a17e7de3b
M010K <1223987303@qq.com>
2021-06-25 21:39:05 +0800

    install django_password_reset module and add email settings

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',  # install blog app
     'comments.apps.CommentsConfig',  # install comments app
-    'userprofile.apps.UserprofileConfig',   # install userprofile app
+    'userprofile.apps.UserprofileConfig',  # install userprofile app
+    'password_reset',   # install password-reset module
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/layer')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+# SMTP服务器，改为你的邮箱的smtp!
+EMAIL_HOST = 'smtp.163.com'
+# 改为你自己的邮箱名！
+EMAIL_HOST_USER = 'mk180400112@163.com'
+# 你的邮箱密码
+EMAIL_HOST_PASSWORD = 'RDLBFUYPCQRABJWR'
+# 发送邮件的端口
+EMAIL_PORT = 25
+# 是否使用 TLS
+EMAIL_USE_TLS = True
+# 默认的发件人
+DEFAULT_FROM_EMAIL = 'M010K的博客 <mk180400112@163.com>'
