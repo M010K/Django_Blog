e059c84466df3cd69741d139760937c3bfb3bb92
M010K <1223987303@qq.com>
2021-06-30 15:37:19 +0800

    separate local and production settings

---
+++ blogproject/settings/local.py
@@ -0,0 +1,9 @@
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'lgw=@j3+_z-6i(v!da^cay-su@_l-fgskv9_1s-wbl@#tr@x1@'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
